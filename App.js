b7835ac44ba7b81950fdb7bd5a9004675d6f8b73
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2024-01-03 07:52:55 +0300

    project setup + initial scaffold

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Issue from "./components/Issue";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.issuesWrapper}>
+        <Text style={styles.sectionTitle}>Issues Logger</Text>
+        <View style={styles.issues}>
+          <Issue />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
   },
+  issuesWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  issues: { marginTop: 30 },
 });
